eadd088917700769eeedec5b5bf6c1adb5a277ee
altponos <artemavx@gmail.com>
2025-04-15 09:14:29 +0300

    lab 11 - main branch change

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,11 +1,10 @@
 ﻿using Newtonsoft.Json.Linq;
 
 JArray array = new JArray();
-array.Add("text");
 array.Add(new DateTime(2005, 1, 11));
 
 JObject o = new JObject();
-o["MyArray"] = array;
+o["NeArray"] = array;
 
 string json = o.ToString();
 Console.WriteLine(json);
